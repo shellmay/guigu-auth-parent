df4c964422764fdf2c3868142fd7301854e5e12e
shellmay <1147258759@qq.com>
2022-11-24 02:00:20 +0800

    异常处理

--- common/service-util/src/main/java/com/atguigu/system/execption/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/atguigu/system/execption/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
-package com.atguigu.system.handler;
+package com.atguigu.system.execption;
 
 import com.atguigu.common.result.Result;
+import com.atguigu.common.result.ResultCodeEnum;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 
 /**
@@ -12,6 +16,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * spring security异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public Result error(AccessDeniedException e) throws AccessDeniedException {
+        return Result.fail().code(ResultCodeEnum.PERMISSION.getCode()).message("没有当前功能权限");
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
